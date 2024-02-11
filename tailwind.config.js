49ea5910ea024c7665278bff9d707be7d533eb94
ccsio <ccsio@protonmail.com>
2024-02-11 15:04:48 +0100

    french translation, celia's photo, google api -> local font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         "icarus-red-1/2": "rgba(136,8,7,0.5)",
         "gray-700-3/4": "rgba(55, 65, 81, 0.75)"
       },
+      fontFamily: {
+        DelaGothic: ["DelaGothic","sans-serif"]
+      }
     },
     screens: {
       sm: {'raw': '(min-width: 640px)'},
