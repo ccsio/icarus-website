f9d2d6fcd3b872beaa7886cd38c0c638b077bef6
Lukáš Hlaváč <ccsio@protonmail.com>
2025-10-12 17:35:22 +0200

    fixed tailwind except landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "index.html",
-    "./test.html",
-    "./**/*.html",
-  ],
-  safelist: [
-    'bg-icarus-red',  
-    'bg-icarus-red-1/2',
-    'bg-gray-700-3/4',
-    'text-icarus-red',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        "black-85": "rgba(0,0,0,0.85)",
-        "icarus-red": "#880807",
-        "icarus-red-59": "rgba(136,8,7,0.5)",
-        "gray-700-75": "rgba(55, 65, 81, 0.75)"
-      },
-      fontFamily: {
-        DelaGothic: ["DelaGothic", "sans-serif"]
-      }
-    },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-  },
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "index.html",
+//     "./test.html",
+//     "./**/*.html",
+//   ],
+//   safelist: [
+//     'bg-icarus-red',  
+//     'bg-icarus-red-1/2',
+//     'bg-gray-700-3/4',
+//     'text-icarus-red',
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         "black-85": "rgba(0,0,0,0.85)",
+//         "icarus-red": "#880807",
+//         "icarus-red-59": "rgba(136,8,7,0.5)",
+//         "gray-700-75": "rgba(55, 65, 81, 0.75)"
+//       },
+//       fontFamily: {
+//         DelaGothic: ["DelaGothic", "sans-serif"]
+//       }
+//     },
+//     screens: {
+//       sm: "640px",
+//       md: "768px",
+//       lg: "1024px",
+//   },
+//   },
+//   plugins: [],
+// };
